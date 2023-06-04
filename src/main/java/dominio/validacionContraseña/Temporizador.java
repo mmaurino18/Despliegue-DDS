5bd284fda636503439993c79bc9b5732be409a4c
mmaurino18 <103010308+mmaurino18@users.noreply.github.com>
2023-06-04 13:37:06 -0300

    Add files via upload

---
+++ src/main/java/dominio/validacionContraseña/Temporizador.java
@@ -0,0 +1,33 @@
+package validacionContraseña;
+import java.util.Scanner;
+
+public class Temporizador {
+    private static final String contra = "Matias1";
+    private static final int intentos_max = 3;
+    private int tiempo_espera = 30;
+
+    public void Usar() throws InterruptedException{
+        Scanner scanner = new Scanner(System.in);
+        int intentos = 0;
+
+        while (intentos < intentos_max) {
+            System.out.print("Ingresa la contraseña: ");
+            String contra_ingresada = scanner.nextLine();
+
+            if (contra_ingresada.equals(contra)) {
+                System.out.println("¡Contraseña correcta!");
+                break;
+            } else {
+                intentos++;
+                System.out.println("Contraseña incorrecta. Intento " + intentos + " de " + intentos_max + ".");
+                if (intentos == intentos_max) {
+                    System.out.println("Demasiados intentos fallidos. Espera " + tiempo_espera + " segundos.");
+                    Thread.sleep(tiempo_espera * 1000);
+                    tiempo_espera =  tiempo_espera* 2;
+                    intentos=0;
+                }
+            }
+        }
+        scanner.close();
+    }
+}
