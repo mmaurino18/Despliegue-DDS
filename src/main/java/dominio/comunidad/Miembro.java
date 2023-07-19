5ed4a8027d9354085253043317d1c972f3f1cd3e
mmaurino18 <103010308+mmaurino18@users.noreply.github.com>
2023-07-19 01:23:58 -0300

    Update Miembro.java

--- src/main/java/dominio/comunidad/Miembro.java
+++ src/main/java/dominio/comunidad/Miembro.java
@@ -85,4 +85,22 @@ public class Miembro {
     public String getNombre() {
         return this.nombre;
     }
+    public static Establecimiento encontrarCoordenadaMasCercana(List<Establecimiento> establecimientos, double x, double y) {
+        Establecimiento establecimientoMasCercano = null;
+        double distanciaMinima = Double.MAX_VALUE;
+    
+        for (Establecimiento establecimiento : establecimientos) {
+            double x2= Establecimiento.getCoordenadaX();
+            double y2 = Establecimiento.getCoordenaY();
+            double distancia = calcularDistancia(x, y,x2,y2);
+            if (distancia < distanciaMinima) {
+                distanciaMinima = distancia;
+                establecimientoMasCercano = establecimiento;
+            }
+        }
+        return establecimientoMasCercano;
+    }
+    public static double calcularDistancia(double x1, double y1, double x2, double y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
 }
