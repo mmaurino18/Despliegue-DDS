83c76353dada11c8416c9d298e340232a376d025
mmaurino18 <103010308+mmaurino18@users.noreply.github.com>
2023-07-19 01:31:37 -0300

    Add files via upload

---
+++ src/main/java/dominio/api/UbicacionResponse.java
@@ -0,0 +1,14 @@
+package api;
+
+public class UbicacionResponse {
+    private Municipio municipio;
+    private Provincia provincia;
+
+    public Municipio getMunicipio() {
+        return municipio;
+    }
+    public Provincia getProvincia() {
+        return provincia;
+    }
+}
+
