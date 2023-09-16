c023b066b08a0924e2cfc0b55d97f4ee0ef8af56
mmaurino18 <matiagus0312@gmail.com>
2023-09-16 03:01:48 -0300

    "Api "

--- src/main/java/dominio/api/FusionComunidades.java
+++ src/main/java/dominio/api/FusionComunidades.java
@@ -21,7 +21,9 @@ import org.json.JSONObject;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 public class FusionComunidades {
@@ -49,69 +51,137 @@ public class FusionComunidades {
         List<Ciudadano> miembros2 = new ArrayList<>();
         miembros2.add(ciudadano);
         miembros2.add(ciudadano2);
-        comunidad.setMiembros(miembros);
+        comunidad.setMiembros(miembros2);
         comunidad2.setMiembros(miembros2);
-        comunidad3.setMiembros(miembros2);
+        comunidad3.setMiembros(miembros);
+        float gradoConfianza = 0.5f ;
+        float gradoConfianza2 = 0.2f ;
+        comunidad.setGradoDeConfianza(gradoConfianza);
+        comunidad2.setGradoDeConfianza(gradoConfianza);
+        comunidad3.setGradoDeConfianza(gradoConfianza2);
         List<Comunidad> comunidades = new ArrayList<>();
         comunidades.add(comunidad);
         comunidades.add(comunidad2);
         comunidades.add(comunidad3);
-        fusionComunidades.sendCommunitiesToApi(comunidades);
+        fusionComunidades.sendCommunitiesToApi(comunidad, comunidades);
         fusionComunidades.mostrarJson("http://localhost:8000/comunidades/fusionables/");
     }
-    public void sendCommunitiesToApi(List<Comunidad> comunidades) {
+    public void sendCommunitiesToApi(Comunidad comunidad, List<Comunidad> comunidades) {
         RestTemplate restTemplate = new RestTemplate();
 
         // Crear encabezados HTTP
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
 
-        // Convertir la lista de comunidades a JSON
-        // Suponiendo que tienes un método `toJson` o similar
-        String jsonCommunities = toJson(comunidades);
-
-        // Crear una entidad HTTP que contenga los encabezados y el cuerpo
-        HttpEntity<String> request = new HttpEntity<>(jsonCommunities, headers);
-
-        // Enviar la solicitud POST
-        String apiResponse = restTemplate.postForObject("http://localhost:8000/comunidades/fusionables/", request, String.class);
-
-
-    }
-
-    public String toJson(List<Comunidad> comunidades) {
+        // Convertir la comunidad y la lista de comunidades a JSON
         ObjectMapper objectMapper = new ObjectMapper();
-        String jsonStr = "";
-
+        Map<String, Object> payload = new HashMap<>();
         try {
-            jsonStr = objectMapper.writeValueAsString(comunidades);
+            payload.put("comunidad", comunidad);
+            payload.put("lista_comunidades", comunidades);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error al agregar comunidad y lista de comunidades al payload");
+            return;
+        }
+
+        String jsonPayload = "";
+        try {
+            jsonPayload = objectMapper.writeValueAsString(payload);
         } catch (JsonProcessingException e) {
             e.printStackTrace();
+            System.out.println("Error al convertir el payload a JSON");
+            return;
         }
-        return jsonStr;
-    }
-    public void mostrarJson(String url1){
-            try {
-                URL url = new URL(url1);
-                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-                conn.setRequestMethod("GET");
-                conn.setRequestProperty("Accept", "application/json");
 
-                if (conn.getResponseCode() != 200) {
-                    throw new RuntimeException("Failed : HTTP Error code : " + conn.getResponseCode());
-                }
+        // Crear una entidad HTTP que contenga los encabezados y el cuerpo
+        HttpEntity<String> request = new HttpEntity<>(jsonPayload, headers);
 
-                InputStreamReader in = new InputStreamReader(conn.getInputStream());
-                BufferedReader br = new BufferedReader(in);
-                String output;
-                while ((output = br.readLine()) != null) {
-                    System.out.println(output);
-                }
-                conn.disconnect();
+        // Enviar la solicitud POST
+        try {
+            ResponseEntity<String> response = restTemplate.postForEntity("http://localhost:8000/propose_fusion/", request, String.class);
 
-            } catch (Exception e) {
-                System.out.println("Exception: " + e);
+            if (response.getStatusCodeValue() == 200) {
+                System.out.println("Solicitud POST exitosa: " + response.getBody());
+            } else {
+                System.out.println("Error en la solicitud POST: " + response.getStatusCodeValue());
             }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Excepción al realizar la solicitud POST");
+        }
     }
 
+
+    public void mostrarJson(String url1){
+        try {
+            URL url = new URL(url1);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("GET");
+            conn.setRequestProperty("Accept", "application/json");
+
+            if (conn.getResponseCode() != 200) {
+                System.out.println("Failed: HTTP Error code: " + conn.getResponseCode());
+                return;
+            }
+
+            InputStreamReader in = new InputStreamReader(conn.getInputStream());
+            BufferedReader br = new BufferedReader(in);
+            String output;
+            while ((output = br.readLine()) != null) {
+                System.out.println(output);
+            }
+            conn.disconnect();
+        } catch (Exception e) {
+            System.out.println("Exception: " + e.getMessage());
+        }
+    }
+    public JSONObject ObtenerJson(String url1){
+        HttpURLConnection conn = null;
+        try {
+            // URL del endpoint
+            URL url = new URL(url1);
+            conn = (HttpURLConnection) url.openConnection();
+
+            // Configuración de la solicitud HTTP
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            conn.setRequestProperty("Accept", "application/json");
+
+            // Aquí deberías escribir el código para enviar el cuerpo de la solicitud
+            // que incluye los detalles de las comunidades a fusionar
+            // ...
+
+            // Leer la respuesta
+            BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            // Convertir la respuesta a JSON
+            JSONObject jsonResponse = new JSONObject(response.toString());
+            return  jsonResponse;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        } finally {
+            if (conn != null) {
+                conn.disconnect();
+            }
+        }
+    }
+    public void fusionarComunidades(){
+       JSONObject comunidadJson = this.ObtenerJson("http://localhost:8000/fuse_communities/");
+        Comunidad nuevaComunidad = new Comunidad();
+        nuevaComunidad.setNombre(comunidadJson.getString("nombre"));
+        nuevaComunidad.setGradoDeConfianza(comunidadJson.getFloat("gradoDeConfianza"));
+        //eliminarComunidad();
+    }
+    public void eliminarComunidad(Comunidad comunidad){
+
+    }
 }
