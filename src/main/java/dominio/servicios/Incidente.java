6c1fe0c92d207aee33b6cefa4c5baf57560dbb65
DanteBall <dntball@gmail.com>
2023-10-17 11:03:47 -0300

    maquetas login e incidentes

--- src/main/java/dominio/servicios/Incidente.java
+++ src/main/java/dominio/servicios/Incidente.java
@@ -7,6 +7,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 
@@ -73,6 +74,10 @@ public class Incidente extends Persistente {
         else {
             return false;
         }
+    }
+
+    public String fechaHora (){
+        return LocalDateTime.of(fechaIncidente,horarioIncidente).toString();
     }
 
     public String estadoIncidente (){
